4e1a2ef0533a70c41bb7c8aa47d1c8df88e3fc2e
Mina <MinaDawood>
2020-08-24 11:07:02 +0200

    Push to Github

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+  loop: false,
+  margin: 20,
+  items: 2,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  navText: [
+    "<i class='fas fa-caret-left p-3 fa-2x text-white'></i>",
+    "<i class='fas fa-caret-right p-3 fa-2x text-white'></i>",
+  ],
+  responsive: {
+    0: {
+      nav: false,
+    },
+    600: {
+      nav: false,
+    },
+    1000: {
+      nav: true,
+    },
+  },
+});
