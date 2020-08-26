834677bdb877647f5ec3cab43ff4381fc42bbfd8
Mina <MinaDawood>
2020-08-26 07:15:14 +0200

    some changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
+/*========== Vanilla Java script Code ==========*/
+
+window.onload = function () {
+  /**
+   * The Page Load First Then The Loader Fades Out And Content Shows
+   */
+  $('.loader').fadeOut();
+};
+
+/*========== JQuery Code ==========*/
+
 $('.owl-carousel').owlCarousel({
   loop: false,
   margin: 20,
   items: 2,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  autoplayHoverPause: true,
+  // autoplay: true,
+  // autoplayTimeout: 3000,
+  // autoplayHoverPause: true,
   navText: [
     "<i class='fas fa-caret-left p-3 fa-2x text-white'></i>",
     "<i class='fas fa-caret-right p-3 fa-2x text-white'></i>",
